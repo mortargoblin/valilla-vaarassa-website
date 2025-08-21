fce95607f5daf01e599754d2b7a206b204827782
mortargoblin <aaro.euro@gmail.com>
2025-08-21 22:57:26 +0300

    wonkify works. content added. revisions.

--- main.js
+++ main.js
@@ -3,13 +3,13 @@
 const navbar = document.querySelector('#navbar');
 const page = document.querySelector('#page')
 
+let loaded = false;
+
 loadPageContent('./home.html');
 
 navbar.addEventListener('click', function(evt) {
-  console.log(evt);
   let target = evt.target.id;
   loadPageContent(target + ".html");
-  wonkify();
 });
 
 
@@ -17,29 +17,26 @@ function loadPageContent(content) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', content, true);
   xhr.onreadystatechange= function() {
-    page.innerHTML= this.responseText;
+    if (this.readyState === 4 && this.status === 200) {
+      page.innerHTML = this.responseText;
+      // specify wonkiness for different pages
+      if (content == 'artistsmap.html') {
+        wonkify(35);
+      } else {
+        wonkify();
+      }
+    }
   };
   xhr.send();
+  wonkify();
 }
 
 // wonkify
-function wonkify(ammount = -10) {
-  const wonkies = document.querySelectorAll('.wonky-textbox');
-  let deg;
+function wonkify(ammount = 10) {
+  const wonkies = document.querySelectorAll('.wonky');
   wonkies.forEach(function(box) {
-    deg = Math.floor(Math.random() * ammount);
+    deg = Math.floor(Math.random() * ammount) - ammount / 2;
     box.style.transform = 'rotate('+ deg +'deg)';
-    console.log(deg, box);
   });
 }
-
-
-
-// once document fully loaded
-document.addEventListener('readystatechange', function(evt) {
-  if(document.readyState === "complete"){
-    console.log(document.readyState);
-    wonkify();
-  }
-});
 
