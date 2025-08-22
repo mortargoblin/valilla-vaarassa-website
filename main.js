dc94ba6a2446404ecbab9da7ed5c48228fed463e
mortargoblin <aaro.euro@gmail.com>
2025-08-22 11:03:44 +0300

    .

--- main.js
+++ main.js
@@ -2,36 +2,47 @@
 
 const navbar = document.querySelector('#navbar');
 const page = document.querySelector('#page')
+const header = document.querySelector('#header')
 
-let loaded = false;
+let headerLoaded = false;
 
-loadPageContent('./home.html');
+loadPageContent('home.html');
 
 navbar.addEventListener('click', function(evt) {
   let target = evt.target.id;
-  loadPageContent(target + ".html");
+  if (target !== 'navbar') {
+    loadPageContent(target + ".html");
+  }
 });
 
-
-function loadPageContent(content) {
+function loadPageContent( content, target=page ) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', content, true);
   xhr.onreadystatechange= function() {
     if (this.readyState === 4 && this.status === 200) {
-      page.innerHTML = this.responseText;
-      // specify wonkiness for different pages
-      if (content == 'artists.html') {
+      target.innerHTML = this.responseText;
+      // conditionally load header
+      if (content === 'home.html') {
+        header.innerHTML = '';
+        headerLoaded = false;
+      } else if (!headerLoaded) {
+        console.log('loading header');
+        loadPageContent('header.html', header);
+        headerLoaded = true;
+      }
+      // specify wonkyness for pages
+      if (content === 'artists.html') {
         wonkify(35);
       } else {
         wonkify();
       }
+
     }
   };
   xhr.send();
   wonkify();
 }
 
-// wonkify
 function wonkify(ammount = 10) {
   const wonkies = document.querySelectorAll('.wonky');
   wonkies.forEach(function(box) {
