eac6acf3709eb17d8edd7998167342d2a075ffc7
mortargoblin <aaro.euro@gmail.com>
2025-09-05 21:20:52 +0300

    artists added

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // main.js
 
 const navbar = document.querySelector('#navbar');
-const page = document.querySelector('#page')
-const header = document.querySelector('#header')
+const page = document.querySelector('#page');
+const header = document.querySelector('#header');
 
-let headerLoaded = false;
+let artistGrid;
+let headerLoaded;
 
 loadPageContent('home.html');
 
@@ -15,7 +16,7 @@ navbar.addEventListener('click', function(evt) {
   }
 });
 
-function loadPageContent( content, target=page ) {
+function loadPageContent(content, target=page) {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', content, true);
   xhr.onreadystatechange= function() {
@@ -30,9 +31,10 @@ function loadPageContent( content, target=page ) {
         loadPageContent('header.html', header);
         headerLoaded = true;
       }
-      // specify wonkyness for pages
+
       if (content === 'artists.html') {
-        wonkify(35);
+        loadArtists();
+        wonkify(40);
       } else {
         wonkify();
       }
@@ -40,14 +42,53 @@ function loadPageContent( content, target=page ) {
     }
   };
   xhr.send();
-  wonkify();
 }
 
-function wonkify(ammount = 10) {
+function wonkify(amount = 10) {
   const wonkies = document.querySelectorAll('.wonky');
   wonkies.forEach(function(box) {
-    deg = Math.floor(Math.random() * ammount) - ammount / 2;
+    deg = Math.floor(Math.random() * amount) - amount / 2;
     box.style.transform = 'rotate('+ deg +'deg)';
   });
 }
 
+function loadArtists() {
+  artistGrid = document.querySelector('#artist-grid');
+  artistGrid.innerHTML = '';
+  const artists = [
+    { fname: 'Zina', lname: 'Marpegan' },
+    { fname: 'Vertti', lname: 'Luostarinen' }, 
+    { fname: 'Vastiala', lname: '- Ylipieti' },
+    { fname: 'Suthasinee', lname: 'Naktnasukajn' }, 
+    { fname: 'Müge', lname: 'YILDIZ' }, 
+    { fname: 'Minne', lname: 'Suoniemi' }, 
+    { fname: 'Markku', lname: 'Laskuj&#228;rvi' }, 
+    { fname: 'Mari', lname: 'Nurmenniemi' }, 
+    { fname: ['Lù', 'Vera'], lname: ['Chén', 'Rantamaa'] }, 
+    { fname: ['Laura', 'Jurgis'], lname: ['Beloff', 'Peters'] }, 
+    { fname: ['Jonna', 'Zina'], lname: ['Eloranta', 'Marpegan'] }, 
+    { fname: 'Joel', lname: 'Rännäri' }, 
+    { fname: 'Henrik', lname: 'Frondelius' }, 
+    { fname: 'Harri', lname: 'Laakso' }, 
+    { fname: 'Gabriella', lname: 'Presnal' }, 
+    { fname: 'Daniel', lname: 'Palpa' }, 
+    { fname: 'Cynthia', lname: 'Blanchette' }, 
+  ]
+  artists.forEach(function(artist) {
+    //console.log(artist.name);
+    console.log(artist.fname, Array.isArray(artist.fname));
+    if (!Array.isArray(artist.fname)) {
+      artistGrid.innerHTML += 
+        `<div id="artist_${artist.fname}" class="wonky">
+          <div> ${artist.fname} </div>
+          <div> ${artist.lname} </div>
+        </div>` 
+    } else {
+      artistGrid.innerHTML += 
+        `<div id="artist_${artist.fname[0]}" class="wonky">
+          <div> ${artist.fname[0]} ${artist.lname[0]}</div>
+          <div> ${artist.fname[1]} ${artist.lname[1]}</div>
+        </div>` 
+    }
+  });
+}
